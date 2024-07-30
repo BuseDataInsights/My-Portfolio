528537422114131529dacd16846ddd3b297c88b8
Buse Keleş <busekeles605@gmail.com>
2024-07-30 15:21:15 +0300

    Create cluster_customers.py
    Add clustering script

---
+++ customer-segmentation/cluster_customers.py
@@ -0,0 +1,18 @@
+import pandas as pd
+from sklearn.cluster import KMeans
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+data = pd.read_csv('Mall_Customers.csv')
+
+X = data[['Annual Income (k$)', 'Spending Score (1-100)']]
+
+kmeans = KMeans(n_clusters=5)
+kmeans.fit(X)
+
+data['Cluster'] = kmeans.labels_
+
+plt.figure(figsize=(10, 6), dpi=300)
+sns.scatterplot(x='Annual Income (k$)', y='Spending Score (1-100)', hue='Cluster', data=data, palette='Set1')
+plt.title('Customer Segments')
+plt.show()
