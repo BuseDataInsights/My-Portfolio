840140b560b4f70b230999bb151b174ecefa56ec
Buse Keleş <busekeles605@gmail.com>
2024-07-30 15:46:58 +0300

    Update cluster_customers.py

--- customer-segmentation/cluster_customers.py
+++ customer-segmentation/cluster_customers.py
@@ -3,26 +3,18 @@ from sklearn.cluster import KMeans
 import matplotlib.pyplot as plt
 import seaborn as sns
 
-# Veri setini yükleme
 data = pd.read_csv('Mall_Customers.csv')
 
-# Gerekli sütunları seçme
 X = data[['Annual Income (k$)', 'Spending Score (1-100)']]
 
-# K-means modeli oluşturma
 kmeans = KMeans(n_clusters=5)
 kmeans.fit(X)
 
-# Grupları ekleme
 data['Cluster'] = kmeans.labels_
 
-# Grupları görselleştirme
 plt.figure(figsize=(10, 6))
 sns.scatterplot(x='Annual Income (k$)', y='Spending Score (1-100)', hue='Cluster', data=data, palette='Set1')
 plt.title('Customer Segments')
 
-# Grafiği kaydetme
 plt.savefig('customer_segments.png')
-
-# Grafiği gösterme
 plt.show()
